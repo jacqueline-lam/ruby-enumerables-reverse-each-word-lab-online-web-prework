4359daab41fa8ca88abf16ef6976df92ef9c51ee
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-09-06 05:34:52 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,12 +8,3 @@ def reverse_each_word(sentence)
   return sentence.split(" ").collect { |word| word.reverse }.join(' ')
 end
 
-
-# ##
-
-
-#   it 'uses collect' do
-#     expect_any_instance_of(Array).to receive(:collect).and_call_original
-#     reverse_each_word("Verifying that collect is being called.")
-#   end
-# end
