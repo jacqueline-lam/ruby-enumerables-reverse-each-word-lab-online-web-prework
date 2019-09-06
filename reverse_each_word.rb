205cbfa4cafe4b55df9554ad6a617fb6c6a9217b
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-09-06 05:37:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,6 +5,11 @@ def reverse_each_word(sentence)
   # end
   # return reversed_word_array.join(' ')
   
+  # "hello poopy world"
+  # 1. .split(" ") => ["hello", "poopy", "world"]
+  # 2. .collect { |word| word.reverse } => ["olleh", "ypoop", "dlrow"]
+  # 3. .join(" ") => "olleh ypoop dlrow"
+  
   return sentence.split(" ").collect { |word| word.reverse }.join(" ")
 end
 
